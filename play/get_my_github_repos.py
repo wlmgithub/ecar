1ec619124836f240bebb0965a6dc5d8aed65b649
wangliming <wangliming@gmail.com>
2018-11-09 22:46:15 -0800

    add --mine

--- play/get_my_github_repos.py
+++ play/get_my_github_repos.py
@@ -4,16 +4,24 @@ import pprint
 
 import requests
 
+import click
+
 BASE_URL = 'https://api.github.com/users/wlmgithub/repos?per_page=100'
 
-
-def main():
+@click.command()
+@click.option('--mine/--no-mine', default=True)
+def main(mine):
   for n in range(1, 3):
     r = requests.get('{}&page={}'.format(BASE_URL, n))
     if r.status_code == 200:
       ret = r.json()
       for o in ret:
-        print(o['name'])
+        if not mine:
+          if o['fork']:
+            print(o['name'])
+        else:
+          if not o['fork']:
+            print(o['name'])
 
 def main0():
   with open(XXX) as fh:
