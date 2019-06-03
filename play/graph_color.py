50385da4fdeb778f26c56a78d6684037d43c1618
wangliming <wangliming@gmail.com>
2019-06-03 13:54:27 -0700

    Add graph_color

---
+++ play/graph_color.py
@@ -0,0 +1,23 @@
+# credit: https://codereview.stackexchange.com/questions/203319/greedy-graph-coloring-in-python
+
+def color_nodes(graph):
+    color_map = {}
+    # Consider nodes in descending degree
+    for node in sorted(graph, key=lambda x: len(graph[x]), reverse=True):
+        neighbor_colors = set(color_map.get(neigh) for neigh in graph[node])
+        color_map[node] = next(
+            color for color in range(len(graph)) if color not in neighbor_colors
+        )
+    return color_map
+
+if __name__ == '__main__':
+  graph = {
+    'a': list('bcd'),
+    'b': list('ac'),
+    'c': list('abdef'),
+    'd': list('ace'),
+    'e': list('cdf'),
+    'f': list('ce')
+  }
+
+  print( color_nodes(graph))
