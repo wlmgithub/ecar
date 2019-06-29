795d31e8384547b5cc9dcdbe3e21b0809b3bba2d
wangliming <wangliming@gmail.com>
2019-06-29 12:43:57 -0700

    quicksort improved

--- play/quicksort.py
+++ play/quicksort.py
@@ -1,4 +1,4 @@
-def quick_sort(array):
+def quick_sort_0(array):
     if len(array) < 2:
         return array
 
@@ -13,17 +13,36 @@ def quick_sort(array):
             right.append(n)
         else:
             middle.append(n)
+    left = quick_sort_0(left)
+    right = quick_sort_0(right)
+
+    return left + middle + right
+
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+
+    pivot = array[0]
+
+    left, right = [], []
+
+    for n in array:
+        if n < pivot:
+            left.append(n)
+        elif n > pivot:
+            right.append(n)
+
     left = quick_sort(left)
     right = quick_sort(right)
 
-    return left + middle + right
+    return left + [pivot] + right
 
 
 if __name__ == '__main__':
     import random
     data = random.sample(range(-100,101), 10)
     print("data:		", data)
+    sorted_data = quick_sort_0(data)
+    print("sorted data:	", sorted_data)
     sorted_data = quick_sort(data)
     print("sorted data:	", sorted_data)
-
-
